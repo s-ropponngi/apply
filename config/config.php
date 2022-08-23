f68fc19c5cde747fe33c3db92dcfc5832ad06287
s-ropponngi <hello.tra.ropponngi@gmail.com>
2022-08-23 17:17:34 +0900

    heroku s3

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ define('DB_USERNAME','b3df6f381eee25');
 define('DB_PASSWORD','8872b555');
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 require_once(__DIR__ .'/../lib/Controller/functions.php');
-require_once(__DIR__ . '/autoload.php');
+// require_once(__DIR__ . '/autoload.php');
 session_start();
 
 $current_uri = 'http://' . $_SERVER["REQUEST_URI"];
